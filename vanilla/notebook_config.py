8596c2098556e7facc33c5b27c6fce7db7dd545e
helgestefan <on@hstes.de>
2025-04-24 10:58:49 +0200

    renames notebook_config.py

--- vanilla/notebook_config.py
+++ vanilla/notebook_config.py
@@ -1,10 +1,11 @@
-import logging
-import warnings
-import pandas as pd
-
-
 def configure_clean_output_for_jupyter_notebook():
+    import logging
+    import warnings
+    import pandas as pd
+
     logging.basicConfig(level=logging.ERROR)
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     pd.set_option('display.max_columns', 6)
     pd.set_option('display.width', 1000)
+
+
