59c25286761115370a8a6e2b0535527d90318f90
helgestefan <on@hstes.de>
2025-07-31 16:38:31 +0200

    enhances colab setup to be able to re-run entire notebook after setting up environment

--- vanilla/conditional_renderer.py
+++ vanilla/conditional_renderer.py
@@ -61,6 +61,7 @@ class ConditionalRenderer:
     def show_plotly(self, fig: 'go.Figure'):
         if self.preparing_for_github:
             export_path = self.tmp_dir / "plot.png"
+            fig.update_layout(height=self.height)
             fig.write_image(str(export_path), width=self.width, height=self.height)
             display(Image(filename=str(export_path)))
         else:
