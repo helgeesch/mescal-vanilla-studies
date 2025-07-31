e71b8ac800a1f40ced18791bac51264d0e50ec73
helgestefan <on@hstes.de>
2025-07-31 16:39:17 +0200

    fixes height layout in show_plotly

--- vanilla/conditional_renderer.py
+++ vanilla/conditional_renderer.py
@@ -61,10 +61,10 @@ class ConditionalRenderer:
     def show_plotly(self, fig: 'go.Figure'):
         if self.preparing_for_github:
             export_path = self.tmp_dir / "plot.png"
-            fig.update_layout(height=self.height)
             fig.write_image(str(export_path), width=self.width, height=self.height)
             display(Image(filename=str(export_path)))
         else:
+            fig.update_layout(height=self.height)
             fig.show()
 
     def show_folium(self, m: 'folium.Map'):
